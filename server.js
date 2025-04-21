561b6a40ac71e6a6bff8758db414e6a33fae7355
Nour Louhichi <nourlouhichi318@gmail.com>
2025-04-21 13:22:44 +0200

    problem 3d android

--- server.js
+++ server.js
@@ -128,7 +128,7 @@ mongoose.connection.on('error', (err) => {
 
 // CORS Configuration
 app.use(cors({
-  origin: 'http://192.168.1.22:3000',  // Allow requests from this origin (adjust if needed)
+  origin: 'http://192.168.43.83:3000',  // Allow requests from this origin (adjust if needed)
   methods: ['GET', 'POST', 'PUT', 'DELETE'],}
 ));
 
